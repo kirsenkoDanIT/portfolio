43afc28eafc4d7e89288f6c26c64eb1578c0978a
kirsenkodanit <a.kirs@ukr.net>
2019-12-06 13:11:39 +0200

    validation registration data

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,23 @@
 const { Router } = require('express');
 const router = Router();
+const { check, validationResult } = require('express-validator/check');
 
-router.get('/', (req, res) => {
-  res.send('User');
-});
+router.post(
+  '/',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Enter valid email').isEmail(),
+    check('password', 'Enter a password, min 5 characters').isLength({ min: 5 })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    res.send('User');
+  }
+);
 
 module.exports = router;
