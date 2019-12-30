4b43091e5cb6c51621c0aad924d8282fe864520f
kirsenkodanit <a.kirs@ukr.net>
2019-12-30 19:49:26 +0200

    post create&delete route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,8 +1,45 @@
 const { Router } = require('express');
 const router = Router();
+const { check, validationResult } = require('express-validator');
 
-router.get('/', (req, res) => {
-  res.send('Post');
-});
+const Profile = require('../../models/Profile');
+const Post = require('../../models/Post');
+const User = require('../../models/User');
+
+const auth = require('../../middleware/auth');
+
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('text', 'Text is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    try {
+      const user = await User.findById(req.user.id);
+      const post = new Post({
+        text: req.body.text,
+        name: user.name,
+        user: req.user.id,
+        avatar: user.avatar
+      });
+
+      await post.save()
+
+      res.json(post)
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).json('Server error');
+    }
+  }
+);
 
 module.exports = router;
