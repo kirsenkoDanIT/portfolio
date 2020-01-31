07e41044f10148ef8fa406b2891de9759127dbd4
kirsenkodanit <a.kirs@ukr.net>
2020-01-31 14:49:29 +0200

    post comments  routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -24,7 +24,7 @@ router.post(
       return res.status(422).json({ errors: errors.array() });
     }
     try {
-      const user = await await User.findById(req.user.id).select('-password');
+      const user = await User.findById(req.user.id).select('-password');
 
       const post = new Post({
         text: req.body.text,
@@ -159,6 +159,71 @@ router.put('/unlike/:id', auth, async (req, res) => {
     console.error(error.message);
     if (error.kind === 'ObjectId') {
       return res.status(404).json({ msg: 'Post not found' });
+    }
+    res.status(500).json('Server error');
+  }
+});
+
+router.post(
+  '/comment/:id',
+  [
+    auth,
+    [
+      check('text', 'Text is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+      const post = await Post.findById(req.params.id);
+
+      const comment = {
+        text: req.body.text,
+        name: user.name,
+        user: req.user.id,
+        avatar: user.avatar
+      };
+
+      post.comments = [comment, ...post.comments];
+
+      await post.save();
+
+      res.json(post.comments);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).json('Server error');
+    }
+  }
+);
+
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    const comment = post.comments.find(
+      comment => comment.id === req.params.comment_id
+    );
+
+    if (!comment) return res.status(404).json({ msg: 'Comment not found' });
+    if (comment.user.toString() !== req.user.id)
+      return res.status(401).json({ msg: 'User not authorised' });
+
+    post.comments = post.comments.filter(
+      comment => comment.id !== req.params.comment_id
+    );
+
+    await post.save();
+
+    res.json(post.comments);
+  } catch (error) {
+    console.error(error.message);
+    if (error.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Comment not found' });
     }
     res.status(500).json('Server error');
   }
